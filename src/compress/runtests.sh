67308d12835ea819c9f4318d2b72b13409d37320
knavero <knavero@gmail.com>
2014-02-17 15:23:07 -0800

    changed runtests.sh

--- src/compress/runtests.sh
+++ src/compress/runtests.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 # set to true if you want to test that category
-testTcbr=false  # tests -tcbr flag options
-testS=false     # tests -s flag option
-testZ=false     # tests .Z compressed files
-testCrazy=false # tests some crazy flag options that no one would ever do
+testTcbrsZ=true  # tests -tcbrs flag options and compressed output
+testCrazy=true # tests some crazy flag options that no one would ever do
 testWithStaleyCompress=true   # tests with Staley's Compress.o
 
 lastTest=9     
@@ -13,44 +11,26 @@ one=0; two=0; three=0; four=0; five=0; six=0; seven=0; eight=0; nine=9
 
 # comment out the numbers you want to exclude from testing
 one=1
-two=2    # large tcbr output (exclude this when running on vogon)
+#two=2    # large tcbr output (exclude this when running on vogon)
 three=3
 four=4
 five=5
-six=6    # large tcbr output (exclude this when running on vogon)
+#six=6    # large tcbr output (exclude this when running on vogon)
 seven=7
-eight=8  # large tcbr output (exclude this when running on vogon)
+#eight=8  # large tcbr output (exclude this when running on vogon)
 nine=9
 
-if $testTcbr; then
+if $testTcbrsZ; then
    for i in `seq 1 $lastTest`; do
       if [ $i -eq $one -o $i -eq $two -o $i -eq $three -o $i -eq $four -o $i -eq $five -o $i -eq $six -o $i -eq $seven -o $i -eq $eight -o $i -eq $nine ]; then
-         echo -e "\nTEST$i -tcbr flags\n"
-         ./a.out -tcbr test$i.in > test$i$kout
-         ./Compress -tcbr test$i.in > test$i.out
+         echo -e "\nTEST$i -tcbrs flags and testing compressed output\n"
+         ./a.out -tcbrs test$i.in > test$i$kout
+         ./Compress -tcbrs test$i.in > test$i.out
          diff test$i$kout test$i.out
-      fi
-   done
-fi
+         diff test$i.in.K test$i.in.Z
 
-if $testS; then
-   for i in `seq 1 $lastTest`; do
-      if [ $i -eq $one -o $i -eq $two -o $i -eq $three -o $i -eq $four -o $i -eq $five -o $i -eq $six -o $i -eq $seven -o $i -eq $eight -o $i -eq $nine ]; then
-         echo -e "\nTEST$i -s flags\n"
          ./a.out -s test$i.in > test$i$kout
          ./Compress -s test$i.in > test$i.out
-         diff test$i$kout test$i.out
-      fi
-   done
-fi
-
-if $testZ; then
-   for i in `seq 1 $lastTest`; do
-      if [ $i -eq $one -o $i -eq $two -o $i -eq $three -o $i -eq $four -o $i -eq $five -o $i -eq $six -o $i -eq $seven -o $i -eq $eight -o $i -eq $nine ]; then
-         echo -e "\nTEST$i .Z compressed files\n"
-         ./a.out test$i.in 
-         ./Compress test$i.in
-         diff test$i.in.K test$i.in.Z
       fi
    done
 fi
@@ -63,55 +43,25 @@ if [ -e Compress ]; then
 fi
 
 echo -e "Checking test#.in.Z..."
-if [ -e test1.in.Z ]; then
-   diff test1.in.Z originalCompressTests/test1.in.Z
-fi
-
-if [ -e test2.in.Z ]; then
-   diff test2.in.Z originalCompressTests/test2.in.Z
-fi
-
-if [ -e test3.in.Z ]; then
-   diff test3.in.Z originalCompressTests/test3.in.Z
-fi
-
-if [ -e test4.in.Z ]; then
-   diff test4.in.Z originalCompressTests/test4.in.Z
-fi
+for i in `seq 1 4`; do
+   if [ -e test$i.in.Z ]; then
+      diff test$i.in.Z originalCompressTests/test$i.in.Z
+   fi
+done
 
 echo -e "Checking test#.in..."
-if [ -e test1.in ]; then
-   diff test1.in originalCompressTests/test1.in
-fi
-
-if [ -e test2.in ]; then
-   diff test2.in originalCompressTests/test2.in
-fi
-
-if [ -e test3.in ]; then
-   diff test3.in originalCompressTests/test3.in
-fi
-
-if [ -e test4.in ]; then
-   diff test4.in originalCompressTests/test4.in
-fi
+for i in `seq 1 4`; do
+   if [ -e test$i.in ]; then
+      diff test$i.in originalCompressTests/test$i.in
+   fi
+done
 
 echo -e "Checking test#.out..."
-if $testS -a [ -e test1.out ]; then
-   diff test1.out originalCompressTests/test1.out
-fi
-
-if $testS -a [ -e test2.out ]; then
-   diff test2.out originalCompressTests/test2.out
-fi
-
-if $testS -a [ -e test3.out ]; then
-   diff test3.out originalCompressTests/test3.out
-fi
-
-if $testS -a [ -e test4.out ]; then
-   diff test4.out originalCompressTests/test4.out
-fi
+for i in `seq 1 4`; do
+   if [ -e test$i.out ]; then
+      diff test$i.out originalCompressTests/test$i.out
+   fi
+done
 
 echo -e "Done!"
 
